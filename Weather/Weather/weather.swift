5680384f91087098fa522847ae98b49296000c6b
spark-02 <>
2024-05-27 13:41:34 +0900

    途中

--- Weather/Weather/weather.swift
+++ Weather/Weather/weather.swift
@@ -15,15 +15,22 @@ protocol YumemiDelegate {
 
 class WeatherDelegate {
     var delegate: YumemiDelegate?
+   
+   let requestJson = """
+{
+    "area": "Tokyo",
+    "date": "2020-04-01T12:00:00+09:00"
+}
+"""
+    
+    
     
     func yumemiDate() {
         do{
-            let fetchWeatherString = try YumemiWeather.fetchWeatherCondition(at: "")
+            let fetchWeatherString = try YumemiWeather.fetchWeather(requestJson)
             delegate?.ymmWeather(type: fetchWeatherString)
         }catch{
         delegate?.showAlert(alert: "エラー1111")
         }
-    
-        
     }
 }
