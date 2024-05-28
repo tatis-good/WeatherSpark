4b552fbfdae116d5ff777cdc4ee1b71c5375447e
spark-02 <>
2024-05-28 13:00:18 +0900

    完成

--- Weather/Weather/ViewController.swift
+++ Weather/Weather/ViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class ViewController: UIViewController, YumemiDelegate {
     
     @IBOutlet weak var weatherImage: UIImageView!
-    
+    @IBOutlet weak var minLabel: UILabel!
+    @IBOutlet weak var maxLabel: UILabel!
     let weatherDelegate = WeatherDelegate()
     
     override func viewDidLoad() {
@@ -21,41 +22,50 @@ class ViewController: UIViewController, YumemiDelegate {
     
     @IBAction func ReloadBtn(_ sender: Any) {
         weatherDelegate.setWeatherType()
-      
     }
-       func setWeatherType(type: String) {
-            
-            var weather = "sunny"
-            var tintcolor = UIColor.red
-            
-            switch type {
-            case "sunny":
-                weather = "sunny"
-                tintcolor = UIColor.red
-            case "cloudy":
-                weather = "cloudy"
-                tintcolor = UIColor.gray
-            case "rainy":
-                weather = "rainy"
-                tintcolor = UIColor.blue
-            default:
-                break
-            }
-            weatherImage.image = UIImage(named: weather)
-            weatherImage.tintColor = tintcolor
-        }
+    func setWeatherType(type: String) {
         
+        var weather = "sunny"
+        var tintcolor = UIColor.red
         
-        @IBAction func closeBtn(_ sender: Any) {
-            dismiss(animated: true)
+        switch type {
+        case "sunny":
+            weather = "sunny"
+            tintcolor = UIColor.red
+        case "cloudy":
+            weather = "cloudy"
+            tintcolor = UIColor.gray
+        case "rainy":
+            weather = "rainy"
+            tintcolor = UIColor.blue
+        default:
+            break
         }
+        weatherImage.image = UIImage(named: weather)
+        weatherImage.tintColor = tintcolor
+    }
+    
+    
+    @IBAction func closeBtn(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    func setmaxTemperature(max: Int) {
+        self.maxLabel.text = String(max)
+    }
+    
+    func setminTemperature(min: Int) {
+        self.minLabel.text = String(min)
+    }
+    
+    
     func setWeatherError(alert: String){
-    let alertController = UIAlertController(title: alert, message: "時間を置いてもう一度お試しください", preferredStyle: .alert)
-    alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
-}))
-    self.present(alertController, animated: true, completion: nil)
-}
+        let alertController = UIAlertController(title: alert, message: "時間を置いてもう一度お試しください", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+        }))
+        self.present(alertController, animated: true, completion: nil)
     }
+}
 
 
 
--- Weather/Weather/weather.swift
+++ Weather/Weather/weather.swift
@@ -11,6 +11,8 @@ import YumemiWeather
 protocol YumemiDelegate {
     func setWeatherType(type: String)
     func setWeatherError(alert: String)
+    func setmaxTemperature(max: Int)
+    func setminTemperature(min: Int)
     }
 
 class WeatherDelegate {
@@ -24,10 +26,22 @@ class WeatherDelegate {
 """
     func setWeatherType() {
         do{
+            
             let fetchWeatherCondition = try YumemiWeather.fetchWeather(requestJson)
+            
+            guard let jsonData = fetchWeatherCondition.data(using: .utf8),
+                  let json = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any],
+                  let maxTemperature = json["max_temperature"] as? Int,
+                  let minTemperature = json["min_temperature"] as? Int,
+                  let fetchWeatherCondition = json["weather_condition"] as? String
+            else{ return
+            }
             delegate?.setWeatherType(type: fetchWeatherCondition)
+            delegate?.setmaxTemperature(max: maxTemperature)
+            delegate?.setminTemperature(min: minTemperature)
         }catch{
         delegate?.setWeatherError(alert: "エラー1111")
+        
         }
     }
 }
